f7afe6e1216bc08df6b804c1cd4ee5cf41489426
anhdo1896 <anhdongoc18@gmail.com>
2021-02-19 11:09:30 +0700

    add bootswatch

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     FormsModule,
     HttpClientModule,
     BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
   ],
   providers: [AuthService, ErrorInterceptorProvider],
   bootstrap: [AppComponent],
