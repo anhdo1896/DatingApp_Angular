5b0fa283442814d3465fb507fcc53fdb08a6e4d8
anhdo1896 <50945992+anhdo1896@users.noreply.github.com>
2021-05-17 08:16:07 +0700

    add pagination

--- DatingApp.API/Helpers/Extensions.cs
+++ DatingApp.API/Helpers/Extensions.cs
@@ -1,5 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
 
 namespace DatingApp.API.Helpers
 {
@@ -8,13 +10,20 @@ namespace DatingApp.API.Helpers
         public static void AddApplicationError(this HttpResponse response, string message)
         {
             response.Headers.Add("Application-Error", message);
-            response.Headers.Add("Access-Control-Expose-Headers","Application-Error");
-            response.Headers.Add("Access-Control-Allow-Origin","*");
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+        public static void AddPagination(this HttpResponse response,
+        int currentPage, int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
         }
         public static int CaculateAge(this DateTime theDateTime)
         {
             int age = DateTime.Today.Year - theDateTime.Year;
-            if(theDateTime.AddYears(age) > DateTime.Today)
+            if (theDateTime.AddYears(age) > DateTime.Today)
                 age--;
             return age;
         }
